f4b7133aad25bfba5b59145fa87d17947010ff9e
Victor Kempfer <victor.kempfer77@gmail.com>
2021-11-21 21:00:57 -0400

    segundo projeto feito com react

---
+++ src/componentes/Mapa/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContatoMapa = () => {
+    return (
+        <div className="contato-rota">
+            <iframe src="https://www.google.com/maps/dir/Cl%C3%ADnica+Prime+Odontologia+Integrada,+Regional+Centro+Comercial+-+Av.+Presidente+Artur+Bernardes,+855+-+Sala+5+e+6+-+Vila+Ipase,+V%C3%A1rzea+Grande+-+MT,+78125-185/Cl%C3%ADnica+Prime+Odontologia+Integrada/@-15.6581386,-56.1210111,18z/data=!4m13!4m12!1m5!1m1!1s0x939dadd1bb598c59:0x7e65d8cf73c3e79e!2m2!1d-56.1231833!2d-15.6594474!1m5!1m1!1s0x939dadd1bb598c59:0x7e65d8cf73c3e79e!2m2!1d-56.1231833!2d-15.6594474?hl=pt-BR" width="100%" height="350" />
+        </div>
+    );
+}
+
+export default ContatoMapa;
---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './style.css';
+
+const Rodape = () => {
+    return (
+        <footer className="rodape">
+            <div className="limitar-container">
+                &copy; Copyright 2021 | Consultório dentes saudáveis
+            </div>
+        </footer>
+    );
+}
+
+export default Rodape;
